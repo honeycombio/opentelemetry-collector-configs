1d373d17fbd3c0180ce7f7b5e8b30a0669bd9097
Mike Goldsmith <goldsmith.mike@gmail.com>
2025-02-25 15:08:56 +0000

    feat: Add new usage processor (#144)
    ## Which problem is this PR solving?
    
    Adds a new usage processor that can calculate the number of bytes for
    each signal request (traces, metrics and logs). These values are not
    recorded anywhere yet and the data is not modified in anyway so it's
    just a pass-through processor.
    
    - https://app.asana.com/0/1208393298994601/1209459312125795

---
+++ usageprocessor/factory.go
@@ -0,0 +1,104 @@
+//go:generate mdatagen metadata.yaml
+
+package usageprocessor
+
+import (
+	"context"
+	"sync"
+
+	"github.com/honeycombio/opentelemetry-collector-configs/usageprocessor/internal/metadata"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/processor"
+	"go.opentelemetry.io/collector/processor/processorhelper"
+)
+
+func NewFactory() processor.Factory {
+	return processor.NewFactory(
+		metadata.Type,
+		createDefaultConfig,
+		processor.WithTraces(createTracesProcessor, component.StabilityLevelDevelopment),
+		processor.WithMetrics(createMetricsProcessor, component.StabilityLevelDevelopment),
+		processor.WithLogs(createLogsProcessor, component.StabilityLevelDevelopment))
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{}
+}
+
+func createTracesProcessor(
+	ctx context.Context,
+	set processor.Settings,
+	cfg component.Config,
+	nextConsumer consumer.Traces,
+) (processor.Traces, error) {
+	processor, err := getOrCreateProcessor(set.ID)
+	if err != nil {
+		return nil, err
+	}
+	return processorhelper.NewTracesProcessor(
+		ctx,
+		set,
+		cfg,
+		nextConsumer,
+		processor.processTraces,
+	)
+}
+
+func createMetricsProcessor(
+	ctx context.Context,
+	set processor.Settings,
+	cfg component.Config,
+	nextConsumer consumer.Metrics,
+) (processor.Metrics, error) {
+	processor, err := getOrCreateProcessor(set.ID)
+	if err != nil {
+		return nil, err
+	}
+	return processorhelper.NewMetricsProcessor(
+		ctx,
+		set,
+		cfg,
+		nextConsumer,
+		processor.processMetrics,
+	)
+}
+
+func createLogsProcessor(
+	ctx context.Context,
+	set processor.Settings,
+	cfg component.Config,
+	nextConsumer consumer.Logs,
+) (processor.Logs, error) {
+	processor, err := getOrCreateProcessor(set.ID)
+	if err != nil {
+		return nil, err
+	}
+	return processorhelper.NewLogsProcessor(
+		ctx,
+		set,
+		cfg,
+		nextConsumer,
+		processor.processLogs,
+	)
+}
+
+var processorsMap = map[component.ID]*usageProcessor{}
+var processorsMux = sync.Mutex{}
+
+func getOrCreateProcessor(id component.ID) (*usageProcessor, error) {
+	processorsMux.Lock()
+	defer processorsMux.Unlock()
+
+	if processor, ok := processorsMap[id]; ok {
+		return processor, nil
+	}
+
+	processor, err := newUsageProcessor()
+	if err != nil {
+		return nil, err
+	}
+
+	processorsMap[id] = processor
+	return processor, nil
+}
